d52e7a8be22e553644afa27f19c1044eefdb8856
brittanymae01 <brittanymae01>
2019-11-15 11:01:03 -0800

    jk now its clean

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -3,10 +3,6 @@ import React from "react";
 export default function LocationCard({ name, type, dimension }) {
   return (
     <div>
-      <h2>{name}</h2>
-      <p>{type}</p>
-      <p>{dimension}</p>
-      <p>Hello</p>
     </div>
   )
 }
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,27 +1,8 @@
 import React from "react";
-// import axios from 'axios'
-// import LocationCard from './LocationCard'
 
 export default function LocationsList() {
-    // const [location, setLocation] = useState([])
-
-    // useEffect(() => {
-    //     axios.get('https://rickandmortyapi.com/api/location/').then(response => {
-    //         console.log(response)
-    //         setLocation(response.data.results)
-    //     })
-    // }, [])
     return (
         <div>
-            {/* <LocationCard />
-            {location.map(location => {
-                <LocationCard
-                    key={location.id}
-                    name={location.name}
-                    type={location.type}
-                    dimension={location.dimension}
-                />
-            })} */}
         </div>
     )
 }
